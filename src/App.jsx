61d2076e2f2c1efda6a7d9905f1ec00e6ad9a47a
samiramrullah <samiramrullah@gmail.com>
2023-04-09 15:47:18 +0530

    superadmin login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-import { Routes, Route } from 'react-router-dom'
 import Error from './pages/Error'
+import Test from './pages/Test'
+import SuperAdminLogin from './components/SuperAdmin/SuperAdminLogin'
+
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/sadminlogin' element={<SuperAdminLogin/>}/>
+        <Route path='/test' element={<Test/>} />
         <Route path='/dashboard/*' element={<Dashboard />} />
         <Route path='*' element={<Error />} />
       </Routes>
