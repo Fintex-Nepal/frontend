79647c01a8d134728ed442865a3886ee9c22981f
samiramrullah <samiramrullah@gmail.com>
2023-04-14 11:32:50 +0530

    Main regestration form fixed

--- src/pages/Admin/MainRegestration.jsx
+++ src/pages/Admin/MainRegestration.jsx
@@ -1,68 +1,131 @@
 import React from 'react'
-
+import { useState } from 'react'
 const MainRegestration = () => {
+    const [activeButton, setActiveButton] = useState('')
     return (
         <>
+            <div class="bg-gray-100  flex flex-wrap items-center justify-center">
+                <div onClick={() => setActiveButton('mainledger')} class="w-52 h-16 relative md:mt-0 mt-4 ">
+                    <img src="https://i.ibb.co/DwNs7zG/Steps.png" alt="step1" class="w-full h-full" />
+                    <div class="absolute w-full flex flex-col px-6 items-center justify-center inset-0 m-0">
+                        <p class="w-full text-sm font-medium leading-4 text-white">Main Ledger</p>
+                        <p class="w-full text-xs mt-1 leading-none text-white">Main Ledger Setup</p>
+                    </div>
+                </div>
+                <div onClick={() => setActiveButton('groupsetup')} class="w-52 h-16 relative md:mt-0 mt-4 ">
+                    <img src="https://i.ibb.co/wNZ4nzy/Steps2.png" alt="step2" class="w-full h-full" />
+                    <div class="absolute w-full flex flex-col px-6 items-center justify-center inset-0 m-0">
+                        <p class="w-full text-sm font-medium leading-4 text-indigo-800">Group Setup</p>
+                        <p class="w-full text-xs mt-1 leading-none text-indigo-800">Fill Data</p>
+                    </div>
+                </div>
+
+            </div>
             <div class="grid grid-cols-1 sm:grid-cols-2 h-screen">
                 <div class="  h-1/2 sm:h-full p-4">
                     <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-                        <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">Account settings</h2>
+                        {activeButton==='mainledger' ? (
+                            <form>
+                                <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">Main Ledger</h2>
+                                <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="username">Account Type</label>
+                                        <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                                            <option>Assets</option>
+                                            <option>Liabilities</option>
+                                        </select>
+                                    </div>
 
-                        <form>
-                            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="username">Account Type</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="emailAddress">Group Name</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">Ledger Name</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">लेजरको नाम</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Entry Date</label>
+                                        <input type='date' class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Sub Account</label>
+
+                                        <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                                            <option>Yes</option>
+                                            <option>No</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">GI Code</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">Deprection Rate</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">हिसाब न</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
                                 </div>
 
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="emailAddress">Group Name</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                <div class="flex justify-end mt-6">
+                                    <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+                                </div>
+                            </form>
+                        ) : (
+                            <form>
+                                <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">Main Ledger</h2>
+                                <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="username">Account Type</label>
+                                        <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                                            <option>Assets</option>
+                                            <option>Liabilities</option>
+                                        </select>
+                                    </div>
+
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="emailAddress">Group Name</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">समुहको नाम</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Entry Date</label>
+                                        <input type='date' class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">GI Code</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+                                    <div>
+                                        <label class="text-gray-700 dark:text-gray-200" for="password">Schedule</label>
+                                        <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    </div>
+
                                 </div>
 
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="password">Ledger Name</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                <div class="flex justify-end mt-6">
+                                    <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
                                 </div>
-
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">लेजरको नाम</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Entry Date</label>
-                                    <input type='date' class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Entry Date</label>
-
-                                    <select class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
-                                        <option>Yes</option>
-                                        <option>No</option>
-                                    </select>
-                                </div>
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="password">GI Code</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="password">Deprection Rate</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
-                                <div>
-                                    <label class="text-gray-700 dark:text-gray-200" for="password">हिसाब न</label>
-                                    <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                                </div>
-                            </div>
-
-                            <div class="flex justify-end mt-6">
-                                <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
-                            </div>
-                        </form>
+                            </form>
+                        )}
                     </section>
                 </div>
                 <div class=" h-1/2 sm:h-full p-4">
-                    <div class="bg-gray-800 text-white px-4 py-2 rounded w-full">
+                    <div class=" text-black bg-white px-4 py-2 rounded w-full">
                         <div class="flex flex-col">
                             <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                                 <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
@@ -97,6 +160,7 @@ const MainRegestration = () => {
                                                         Schedule
                                                     </th>
                                                 </tr>
+
                                             </thead>
                                             <tbody>
                                                 <tr class="border-b dark:border-neutral-500">
@@ -121,6 +185,7 @@ const MainRegestration = () => {
                                                         Otto
                                                     </td>
                                                 </tr>
+
                                             </tbody>
                                         </table>
                                     </div>
