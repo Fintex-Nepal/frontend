f0fae8306329312ab5f2d3da542348b97dc5d8cd
samiramrullah <samiramrullah@gmail.com>
2023-04-18 02:48:22 +0530

    responsive dashboard added

--- src/pages/Admin/StaffInfo.jsx
+++ src/pages/Admin/StaffInfo.jsx
@@ -2,34 +2,39 @@ import React from 'react'
 import EmployeeCard from '../../utils/EmployeeCard'
 
 const StaffInfo = () => {
-    const employeeData=[
+    const employeeData = [
         {
-           name:'Samir Alam',
-           designation:'CEO',
-           place:'Birgunj',
-           email:'samiramrullah@gmail.com',
+            name: 'Samir Alam',
+            designation: 'CEO',
+            place: 'Birgunj',
+            email: 'samiramrullah@gmail.com',
         },
         {
-            name:'Ashish',
-            designation:'CEO',
-            place:'Chitwan',
-            email:'ashihish@gmail.com',
-         },
-         
-         {
-            name:'Sajid',
-            designation:'CEO',
-            place:'Birgunj',
-            email:'sajid@gmail.com',
-         }
+            name: 'Ashish',
+            designation: 'CEO',
+            place: 'Chitwan',
+            email: 'ashihish@gmail.com',
+        },
+
+        {
+            name: 'Sajid',
+            designation: 'CEO',
+            place: 'Birgunj',
+            email: 'sajid@gmail.com',
+        }
     ]
     return (
         <>
+            <div class="flex justify-between">
+                <div class="left-div">Left div</div>
+                <div class="right-div">Right div</div>
+            </div>
+
             <section class="flex items-center   font-poppins dark:bg-gray-900 ">
                 <div class="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
                     <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 md:grid-cols-2">
-                        {employeeData?.map((itm=>(
-                            <EmployeeCard name={itm.name} designation={itm.designation} place={itm.place} email={itm.email}/>
+                        {employeeData?.map((itm => (
+                            <EmployeeCard name={itm.name} designation={itm.designation} place={itm.place} email={itm.email} />
                         )))}
 
                     </div>
