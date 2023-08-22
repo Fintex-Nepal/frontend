db102eb49aed8a2d616e185200fcf60491ab8e62
samiramrullah <samiramrullah@gmail.com>
2023-08-22 21:37:39 +0530

    dashboard pointer added

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -178,12 +178,12 @@ const Dashboard = () => {
                                 </li>
 
                                 <li>
-                                    <Link to={'profile'} class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group">
+                                    <Link to={'profile'} class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 group cursor-pointer">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                         </svg>
                                         <span class="flex-1 ml-3 whitespace-nowrap">Profile</span>
-                                        {/* <span class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 ">Pro</span> */}
+                                        
                                     </Link>
                                 </li>
                                 <Dropdown dropDownList={depositDropDown} heading="Deposit" svg={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
